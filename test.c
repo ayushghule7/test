9900d53c439b9b1e260238fc7f6cc98e22f91179
Ayush Ghule <ayushghule7@gmail.com>
2025-05-09 14:55:36 +0530

    added c file

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    printf("Prime numbers between 0 and 50 are:\n");
+    for (int i = 0; i <= 50; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
